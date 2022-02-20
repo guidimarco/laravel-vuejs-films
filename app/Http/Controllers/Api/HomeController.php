d00aaca622a2b8f80b15d328775df1b91bc794d1
Marco Guidi <mark.88g@gmail.com>
2022-02-20 13:33:17 +0100

    Added Guzzle and API getFilm logic

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -5,14 +5,36 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
-    public function getFilm()
+    public function getFilm(Request $request)
     {
-        return response()->json([
-            'success' => true
-        ]);
+        $title = $request->query('title');
+
+        $apiEndpoin = 'https://www.omdbapi.com';
+        $apiParams = [
+            'apiKey' => '72dce447',
+            't' => $title
+        ];
+
+        try {
+            $client = new \GuzzleHttp\Client(['verify' => false ]);
+            $response = $client->request('GET', $apiEndpoin, ['query' => $apiParams]);
+       
+            $statusCode = $response->getStatusCode();
+            $responseBody = json_decode($response->getBody(), true);
+
+            return response()->json([
+                'success' => true,
+                'titleDetails' => $responseBody
+            ]);    
+        } catch (exception $e) {
+            return response()->json([
+                'success' => false
+            ]);
+        }
     }
 }
