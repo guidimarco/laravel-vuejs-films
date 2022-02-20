b0a02f665c37a85e0910fac465201fd3e03c3dfc
Marco Guidi <mark.88g@gmail.com>
2022-02-20 17:33:37 +0100

    Added film-user binding

--- app/Film.php
+++ app/Film.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Film extends Model
 {
-    //
+    protected $fillable=['restaurant_id', 'title', 'year', 'director'];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
 }
