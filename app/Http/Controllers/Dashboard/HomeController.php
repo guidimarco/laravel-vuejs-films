c89378359dfcb5e35bcee6d4c46a30653b68a560
Marco Guidi <mark.88g@gmail.com>
2022-02-20 17:40:56 +0100

    Added validation and DB

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Film;
 
 class HomeController extends Controller
 {
@@ -20,6 +22,21 @@ class HomeController extends Controller
      */
     public function store(Request $request)
     {
+        /* Validation */
+        $request->validate([
+            'title' => 'required|max:50',
+            'year' => 'required|max:50',
+            'director' => 'required|max:50'
+        ]);
+        $form_data = $request->all();
+
+        /* Create new film and save it */
+        $new_restaurant = new Film();
+        $new_restaurant->fill($form_data);
+        $user_id = Auth::user()->id;
+        $new_restaurant->user_id = $user_id;
+        $new_restaurant->save();
+
         return redirect()->route('dashboard.home');
     }
 }
