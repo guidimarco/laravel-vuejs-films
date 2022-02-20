a3b1e372088b3a4371b5f05c9ba8ea033e342427
Marco Guidi <mark.88g@gmail.com>
2022-02-20 17:30:04 +0100

    Added migration

---
+++ database/migrations/2022_02_20_161306_create_films_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->string('year');
+            $table->string('director');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('films', function (Blueprint $table) {
+            $table->dropForeign('films_user_id_foreign');
+        });
+
+        Schema::dropIfExists('films');
+    }
+}
